f8e085e37717c3ca9e2d5cbcb2d8d0f81a75a6d8
irfanahmadhidayat <hidayatirfanahmad7@gmail.com>
2025-06-29 22:31:06 +0700

    add laravel breeze for auth and configuration adminlte 3

---
+++ resources/views/layouts-page/sidebar.blade.php
@@ -0,0 +1,92 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/adminlte" class="brand-link text-center">
+        <span class="brand-text font-weight-bold">AGUS MART</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- DASHBOARD -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <!-- DATA MASTER -->
+                <li class="nav-header">DATA MASTER</li>
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-box"></i>
+                        <p>
+                            Barang
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">
+                                <i class="fas fa-tag nav-icon"></i>
+                                <p>Nama Barang</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="fas fa-th-list nav-icon"></i>
+                                <p>Jenis Barang</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="fas fa-ruler-combined nav-icon"></i>
+                                <p>Satuan Barang</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- TRANSAKSI -->
+                <li class="nav-header">TRANSAKSI</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-arrow-down"></i>
+                        <p>Barang Masuk</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-arrow-up"></i>
+                        <p>Barang Keluar</p>
+                    </a>
+                </li>
+
+                <!-- LAPORAN -->
+                <li class="nav-header">LAPORAN</li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-clipboard-list"></i>
+                        <p>Stok Barang</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-file-import"></i>
+                        <p>Laporan Barang Masuk</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-file-export"></i>
+                        <p>Laporan Barang Keluar</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
